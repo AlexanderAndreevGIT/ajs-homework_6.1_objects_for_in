78d821120ae637e4ff8b4dadddfa84086faa1423
AlexanderAndreevGIT <andreev-alex@inbox.ru>
2021-11-11 15:02:02 +0300

    for...in instead for.Each

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,23 @@ export default function orderByProps(obj, order) {
   const inputObject = { ...obj };
   const result = [];
 
-  order.forEach((prop) => {
-    if (Object.prototype.hasOwnProperty.call(inputObject, prop)) {
-      result.push({ key: prop, value: obj[prop] });
-      delete inputObject[prop];
+  // order.forEach((prop) => {
+  //   if (Object.prototype.hasOwnProperty.call(inputObject, prop)) {
+  //     result.push({ key: prop, value: obj[prop] });
+  //     delete inputObject[prop];
+  //   } else {
+  //     throw new Error(`Пераметр ${prop} не существет`);
+  //   }
+  // });
+
+  for (const prop in order) {
+    if (Object.prototype.hasOwnProperty.call(inputObject, order[prop])) {
+      result.push({ key: order[prop], value: obj[order[prop]] });
+      delete inputObject[order[prop]];
     } else {
-      throw new Error(`Пераметр ${prop} не существет`);
+      throw new Error(`Пераметр ${order[prop]} не существет`);
     }
-  });
+  }
 
   const keysOrderInRest = Object.keys(inputObject);
 
